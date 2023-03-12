e4b47a7183660aa8957084967f5102fe98d73f27
maks075 <shlapak.maksym@gmail.com>
2023-03-11 21:28:48 -0600

    Done

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    
+    
+    
+    let list = document.getElementById("faultyItems");
+    // list.style.visibility = "hidden";
 
+   let form = document.querySelector("form");
+ 
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+       let pilot = document.querySelector("input[name=pilotName]").value;
+      // let pilot = pilotInput.value;
+
+       let copilot = document.querySelector("input[name=copilotName]").value;
+       //let copilot = copilotInput.value;
+
+       let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+      // let fuelLevel = Number(fuelInput.value);
+
+       let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+       //let cargoLevel = Number(cargoInput.value);
+
+       formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+
+});
+    
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let selectedPlanet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
    })
+
+   
+
+   
+   
+   
    
 });
